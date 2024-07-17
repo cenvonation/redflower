241b35bc5f22d981e4997e80d365bbe8b0bcc474
25freepb <benbornapersonal@gmail.com>
2024-07-16 20:02:52 -0400

    add platform-tools

--- first-setup.py
+++ first-setup.py
@@ -1,20 +1,33 @@
 #                                        M A D E  B Y  @stolefromcloudflare
 # T . M E / R E D M I 1 0 _ C O M M U N I T Y  |  T . M E / R E D M I 1 0 _ U P D A T E S  |  T . M E / R 1 0 R O M S
 
-#https://dl.google.com/android/repository/platform-tools-latest-windows.zip
+
 
 import subprocess
+import os
+import shutil
+import zipfile
 
 print("Starting")
-adbprompt = input("Do you want to install platform_tools (recommended) [yes, no]: ")
-if adbprompt == "yes":
-    aria = "cd bin"
-    dir = "dir"
-    print("Starting Download... Please be patient")
 
-    subprocess.Popen(aria, shell=True)
-    subprocess.Popen(dir, shell=True)
-elif adbprompt == "no":
-    print("Skipping...")
-else:
-    print("Unknown command. Rerun script to restart?")
+##############
+#CODE ARCHIVE#
+##############
+
+#adbprompt = input("Do you want to install platform_tools (recommended) [yes, no]: ")
+#if adbprompt == "yes":
+#    print("Starting Download... Please be patient")
+#
+#    os.chdir('bin')
+#    os.chdir("aria2c")
+#    shutil.rmtree("dl")
+#    os.mkdir("dl")
+#    adb = "aria2c -d dl https://dl.google.com/android/repository/platform-tools-latest-windows.zip"
+#    subprocess.Popen(adb, shell=True)
+#    os.chdir("dl")
+#    os.mkdir("extracted")
+#    tar = "tar -xf platform-tools-latest-windows.zip -C extracted"
+#elif adbprompt == "no":
+#    print("Skipping...")
+#else:
+#    print("Unknown command. Rerun script to restart?")
